8da705fd4aea17723b163c36157490dc74f60d49
Pablo Sole <pablo.sole@gmail.com>
2012-05-23 18:01:07 -0300

    updated v8 project to add the two new files we need to build the tool.

--- test.bat
+++ test.bat
@@ -1,5 +1,4 @@
 @echo off
 
-cd build\Release
-..\..\third_party\pin-2.11-49306-msvc9-ia32_intel64-windows\pin.bat -tool_exit -tool_exit_timeout 10 -t pet.dll -- cmd.exe
-cd ..\..\
+..\..\third_party\pin-2.11-49306-msvc9-ia32_intel64-windows\pin.bat -tool_exit -tool_exit_timeout 1 -t pet.dll -- cmd.exe
+
