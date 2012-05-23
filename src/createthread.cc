9f88b7c5623e5f26bfa44ed2223c69952947a0b5
Pablo Sole <pablo.sole@gmail.com>
2012-05-23 17:58:34 -0300

    avoid a PIN spinlock by retrieving the address of the function just the first time the dll is loaded.
    It's also more performant, although I don't expect to run this function many times.

--- src/createthread.cc
+++ src/createthread.cc
@@ -13,6 +13,8 @@ typedef WINDOWS::HANDLE (WINAPI *MyCreateThread_type)(
         WINDOWS::LPDWORD lpThreadId
   );
 
+static MyCreateThread_type MyCreateThread = 0;
+
 extern "C" {
 _declspec(dllexport) WINDOWS::HANDLE WINAPI CreateThread(
         WINDOWS::LPSECURITY_ATTRIBUTES lpThreadAttributes,
@@ -22,17 +24,24 @@ _declspec(dllexport) WINDOWS::HANDLE WINAPI CreateThread(
         WINDOWS::DWORD dwCreationFlags,
         WINDOWS::LPDWORD lpThreadId
   ) {
-	  MyCreateThread_type MyCreateThread;
-
-	  //XXX: maintain updated with the name of the pintool DLL
-	  WINDOWS::HMODULE v8 = WINDOWS::GetModuleHandle("pet.dll");
-	  if (!v8)
-		  return 0;
-
-	  MyCreateThread = (MyCreateThread_type) WINDOWS::GetProcAddress(v8, "MyCreateThread");
-	  if (!MyCreateThread)
-		  return 0;
-
 	  return MyCreateThread(lpThreadAttributes, dwStackSize, lpStartAddress, lpParameter, dwCreationFlags, lpThreadId);
 }
+
+WINDOWS::BOOL WINAPI DllMain(WINDOWS::HANDLE hinstDLL, WINDOWS::DWORD dwReason, WINDOWS::LPVOID lpvReserved) {
+	if (!MyCreateThread)
+	{
+		//XXX: maintain updated with the name of the pintool DLL
+		WINDOWS::HMODULE v8 = WINDOWS::GetModuleHandle("pet.dll");
+		if (!v8)
+		  return 0;
+
+		MyCreateThread = (MyCreateThread_type) WINDOWS::GetProcAddress(v8, "MyCreateThread");
+		if (!MyCreateThread)
+		  return 0;
+	}
+
+	return 1;
 }
+
+}
+
