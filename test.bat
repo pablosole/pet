7f24b3366f382305910ba162ae6407dfb6d0444d
Pablo Sole <pablo.sole@gmail.com>
2012-06-16 02:32:33 -0300

    updated test.bat, like the previous version, put it inside build/Release and execute from there

--- test.bat
+++ test.bat
@@ -1,4 +1,13 @@
 @echo off
 
-..\..\third_party\pin-2.11-49306-msvc9-ia32_intel64-windows\pin.bat -tool_exit -tool_exit_timeout 1 -t pet.dll -- cmd.exe
+set options=
+set tooloptions=
+
+if "%1"=="-d" set options=%options% -pause_tool 5
+if "%2"=="-d" set options=%options% -pause_tool 5
+
+if "%1"=="-v" set tooloptions=%tooloptions% -e "--log-all --print-all-code --trace-lazy --trace-ic --trace-exception --trace-bailout --trace-opt_verbose --trace --code-comments --print-source --print-builtin-source --print-ast --print-builtin-ast"
+if "%2"=="-v" set tooloptions=%tooloptions% -e "--log-all --print-all-code --trace-lazy --trace-ic --trace-exception --trace-bailout --trace-opt_verbose --trace --code-comments --print-source --print-builtin-source --print-ast --print-builtin-ast"
+
+..\..\third_party\pin-2.11-49306-msvc9-ia32_intel64-windows\pin.bat %options% -t pet.dll %tooloptions% -- cmd.exe
 
